0c3b5ab53cc09897a55681b56e2b8e808a760810
Marcos Peralta <mrcperalta.mp@gmail.com>
2017-11-19 23:56:00 -0300

    SE verifica si el usuario esta logueado o no para continuar

--- app/src/main/java/com/vacunas/activity/LoginActivity.java
+++ app/src/main/java/com/vacunas/activity/LoginActivity.java
@@ -3,9 +3,14 @@ package com.vacunas.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -14,6 +19,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.vacunas.R;
 import com.vacunas.rest.ApiBuilder;
@@ -29,6 +35,8 @@ import static android.content.ContentValues.TAG;
 public class LoginActivity extends AppCompatActivity implements Callback<Respuesta<Usuario>>  {
     private static final int RC_SIGN_IN = 9001;
     private GoogleSignInClient mGoogleSignInClient;
+    private ProgressBar mProgressView;
+    private LinearLayout contentLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,19 +46,28 @@ public class LoginActivity extends AppCompatActivity implements Callback<Respues
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
-
-        // Build a GoogleSignInClient with the options specified by gso.
-
-        
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         SignInButton signInButton = findViewById(R.id.sign_in_button);
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //signOut();
+
+                showProgress(true);
+                signOut();
                 googleSignIn();
             }
         });
+
+        contentLogin = (LinearLayout) findViewById(R.id.content);
+        mProgressView = (ProgressBar) findViewById(R.id.progressbar_login);
+        GoogleSignInAccount loggedAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(loggedAccount != null){
+            ApiBuilder.build().getUsuario(loggedAccount.getEmail()).enqueue(LoginActivity.this);
+        }else{
+            showProgress(false);
+        }
     }
     private void googleSignIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
@@ -71,56 +88,35 @@ public class LoginActivity extends AppCompatActivity implements Callback<Respues
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
-            // Signed in successfully, show authenticated UI.
-
-
-            updateUI("SUCCESS" , account );
-
             ApiBuilder.build().getUsuario(account.getEmail()).enqueue(LoginActivity.this);
         } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            showProgress(false);
             Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            //updateUI("FAILED" , null);
         }
-    }
-
-    private void updateUI(String o, GoogleSignInAccount account) {
-        Toast.makeText(LoginActivity.this, "Login " + o + ", cuenta: "+account.getDisplayName() +", correo: "+ account.getEmail(), Toast.LENGTH_SHORT).show();
-        //Log.i(TAG, "Login " + o + ", cuenta: "+account.getDisplayName() +", correo: "+ account.getEmail());
     }
 
     @Override
     public void onResponse(Call<Respuesta<Usuario>> call, Response<Respuesta<Usuario>> response) {
         if (response.isSuccessful()) {
             if (response.body().respuesta != null ) {
-                // Cliente registrado
-                //showProgress(false);
                 Usuario usuario = response.body().respuesta;
                 String idUsuario = usuario.getIdUsuario().toString();
                 String nroDocumento = usuario.getDocumento();
                 String nombre = usuario.getNombres() + " " + usuario.getApellidos();
+                Toast.makeText(LoginActivity.this, "Bienvenido "+ nombre, Toast.LENGTH_SHORT).show();
                 String correo = usuario.getCorreoElectronico();
 
                 Intent i = new Intent(getBaseContext(), HijosActivity.class);
                 i.putExtra(HijosActivity.ID_PADRE, idUsuario);
                 startActivity(i);
                 finish();
-                //String telefono = usuario.get();
-                /*if (isGoogleLogin) {
-                    preferenceUtils.loginGoogle(userId, nroDocumento, nombre, correo, telefono, photoUrl);
-                } else {
-                    preferenceUtils.loginFacebook(userId, nroDocumento, nombre, correo, telefono, photoUrl);
-                }
-                Intent i = new Intent(getBaseContext(), MainActivity.class);
-                startActivity(i);
-                finish();*/
             } else {
                 Toast.makeText(LoginActivity.this, "El usuario no existe", Toast.LENGTH_SHORT).show();
+                showProgress(false);
             }
         } else {
             Toast.makeText(LoginActivity.this, "Error al consultar datos del usuario", Toast.LENGTH_SHORT).show();
+            showProgress(false);
         }
     }
 
@@ -128,5 +124,18 @@ public class LoginActivity extends AppCompatActivity implements Callback<Respues
     public void onFailure(Call<Respuesta<Usuario>> call, Throwable t) {
         Toast.makeText(LoginActivity.this, "Error al consultar datos del usuario", Toast.LENGTH_SHORT).show();
 
+    }
+    public void showProgress(final boolean show) {
+        mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        contentLogin.setVisibility(show ? View.GONE : View.VISIBLE);
+    }
+    public void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
     }
 }
--- app/src/main/java/com/vacunas/rest/ApiBuilder.java
+++ app/src/main/java/com/vacunas/rest/ApiBuilder.java
@@ -1,18 +1,10 @@
 package com.vacunas.rest;
 
-import java.io.IOException;
-
-import okhttp3.Credentials;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import static com.vacunas.Configuraciones.PASSWORD_SERVICE;
 import static com.vacunas.Configuraciones.URL_BASE_SERVICE;
-import static com.vacunas.Configuraciones.USER_NAME_SERVICE;
 
 
 
